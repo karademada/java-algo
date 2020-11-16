c6be8b5114308c5386249a24e9a57ff36090bcbb
lazantsy <christophe.lazantsy@gmail.com>
2020-11-16 08:03:37 +0100

    add files

---
+++ Chap4Exo.java
@@ -0,0 +1,28 @@
+public class Chap4Exo {
+
+  public static void main(String[] args) {
+    int x = 1;
+    int y = 2;
+    int z = 0;
+    int n = 10;
+    int c = 4;
+    int d = 4;
+    int a = 4;
+    int b = 4;
+
+    /** int result = (2 * x) - (int)Math.pow(3, y);
+		Terminal.ecrireIntln(result);**/
+
+    /* int result = (3 + x) + (y - z);
+		Terminal.ecrireIntln(result); */
+
+    /* boolean result = (1 <= x) || (x <= n);
+		Terminal.ecrireBoolean(result); */
+		
+    /* int b = c + d;
+    int a = b;
+	Terminal.ecrireIntln(a); */
+	
+	Terminal.ecrireBoolean(a != b);
+  }
+}
